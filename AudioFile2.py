8f358c65dedc5298dea826e73e620f9f9626e5a2
kya21671 <60804956+kya21671@users.noreply.github.com>
2021-04-17 21:11:14 -0500

    Add files via upload

---
+++ AudioFile2.py
@@ -0,0 +1,12 @@
+from playsound import playsound
+
+def AudioImport(audioFile):
+    nameMusic = audioFile + '.mp3'
+    return playsound(nameMusic)
+
+def returnPhoto(photoName):
+    _____
+
+def main:
+    AudioImport(matchFaceGetArtistName(_____))
+    
